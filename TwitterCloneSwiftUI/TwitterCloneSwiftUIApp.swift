53515b970d764f10899fb6d24d4a2c5b15e9f029
NomoteteS <nomotetes.onetrue@icloud.com>
2022-11-24 11:06:58 +0200

    Initial Commit

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by NomoteteS on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
