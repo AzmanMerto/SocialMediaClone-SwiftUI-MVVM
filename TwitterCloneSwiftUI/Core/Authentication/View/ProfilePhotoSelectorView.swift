f18442146a3652c66fa8d8aead8f2c6cea26dbc0
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-03 14:20:19 +0200

    Ready for ImageAdd

--- TwitterCloneSwiftUI/Core/Authentication/View/ProfilePhotoSelectorView.swift
+++ TwitterCloneSwiftUI/Core/Authentication/View/ProfilePhotoSelectorView.swift
@@ -8,23 +8,45 @@
 import SwiftUI
 
 struct ProfilePhotoSelectorView: View {
+    @State private var showImagePicker = false
+    @State private var selectedImage : UIImage?
+    @State private var profileImage : Image?
     var body: some View {
         VStack{
             AuthHeaderView(title1: "Setup account", title2: "Add a profile photo")
             
             Button {
-                
+                showImagePicker.toggle()
             } label: {
-                Image("plus-icon")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 128, height: 128)
-                    .padding(.top,44)
-            }
-
-            
+                if let profileImage = profileImage {
+                    profileImage
+                        .resizable()
+                        .modifier(ProfileImageModifier())
+                }else {
+                    Image("plus-icon")
+                        .resizable()
+                        .modifier(ProfileImageModifier())
+                        
+                }
+            }.sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+                ImagePicker(selectedImage: $selectedImage)
+            }.padding(.top,44)
+                
             Spacer()
         }.ignoresSafeArea()
+    }
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        profileImage = Image(uiImage: selectedImage)
+    }
+}
+
+private struct ProfileImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 128, height: 128)
+            .clipShape(Circle())
     }
 }
 
